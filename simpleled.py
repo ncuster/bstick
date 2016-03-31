6c6fda37188ecb285d120047c5f9866e50672a6c
Nate Custer <nate.custer@oracle.com>
2016-03-30 21:51:40 -0500

    Added some more error checking, and smarter lighting logic
    The blinkstick has onboard storage, so added some state info to that
    storage so we don't have to iterate over all 32 LEDs if they aren't
    already lit in our scenario.  See the script's doc for more info.

--- simpleled.py
+++ simpleled.py
@@ -1,28 +1,62 @@
 #!/usr/local/bin/python
-# takes two args: max led to light up, and the color to use
-# it will turn off all leds > max led to light up
-# specify color of "off" to turn off the LEDs
+# takes two args: 
+#   argv[1]:  max LED to light up in the strip of LEDs
+#   argv[2]:  color to use to light the LEDs, "off" to turn them off
 #
-# this is super-simple and just very basic, but still some have
-# some basic error checking added at some point.   right now i
-# just use it to tell me how many IMs i have unread by putting
-# in a simple trigger on IM signal.
+# This is just a super-simple script to allow other monitoring types
+# of apps to use this and indicate things for me on the blinkstick.
+# See http://www.blinkstick.com
+#
+# The blinkstick has a very small amount of storage (something like 32 bytes)
+# so we'll store how many LEDs are lit in this block, so we don't have to
+# do too much LED manipulation on LEDs that are already off.  I'm not sure
+# how much this saves us, I just wanted to play w/ the onboard storage.
 
 import sys
 import time
 from blinkstick import blinkstick
 
 MAX_NUM_LEDS = 32
-
 bstick = blinkstick.find_first()
-highestLEDToLight = int(sys.argv[1])
-ledColor = str(sys.argv[2])
 
-for x in range(0, MAX_NUM_LEDS):
-    if x < highestLEDToLight:  
-        bstick.set_color(channel=0, index=x, name=ledColor)
+def usage():
+    print "Usage:  simpleled.py max_leds_to_light color"
+    print "where color is any named color like red, blue, etc., off to turn them off"
+
+def lightLEDs(ledsToLight, maxLEDs, color):
+    try:
+        for x in range(0, maxLEDs):
+            if x < ledsToLight:
+                bstick.set_color(channel=0, index=x, name=color)
+                #bstick.blink(channel=0, index=x, name=color, repeats=100000000)
+            else:
+                bstick.set_color(channel=0, index=x, name='#000000')
+
+            time.sleep(.001)  # it seems if we don't have a minimal sleep blinkstick goes missing
+
+        bstick.set_info_block1(str(ledsToLight));
+        print "Stored " + str(ledsToLight) + " in on-strip memory"
+    except KeyboardInterrupt:
+        #bstick.off()
+        return
+
+
+
+
+def main():
+    if len(sys.argv) < 3:
+        usage()
+        sys.exit(1)
+
+    highestLEDToLight = int(sys.argv[1])
+    ledColor = str(sys.argv[2])
+
+    storedLEDsLit = bstick.get_info_block1()
+
+    if storedLEDsLit < MAX_NUM_LEDS:
+        lightLEDs(highestLEDToLight, storedLEDsLit, ledColor)
     else:
-        bstick.set_color(channel=0, index=x, name='#000000')
+        lightLEDs(highestLEDToLight, MAX_NUM_LEDS, ledColor)
 
-    time.sleep(.001)  # it seems if we don't have a minimal sleep blinkstick goes missing
-    
+if __name__ == "__main__":
+    sys.exit(main())
